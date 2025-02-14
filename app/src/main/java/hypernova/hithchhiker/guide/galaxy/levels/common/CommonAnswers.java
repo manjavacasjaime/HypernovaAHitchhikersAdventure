a94d3b8859295c53c298e13fedffd1e0fdae2923
manjavacasjaime <manjavacasjaime@gmail.com>
2025-02-14 20:11:28 +0100

    add where is answer to CommonAnswers

--- app/src/main/java/hypernova/hithchhiker/guide/galaxy/levels/common/CommonAnswers.java
+++ app/src/main/java/hypernova/hithchhiker/guide/galaxy/levels/common/CommonAnswers.java
@@ -179,6 +179,9 @@ public class CommonAnswers extends AppCompatActivity {
         } else if (myObjX.contains("where") && myObjX.contains("am") && myObjX.contains("i")) {
             secondText.setText("YOUR location is written at the top of the screen.");
             vm.linearLayout.addView(secondText);
+        } else if (myObjX.contains("where is")) {
+            secondText.setText("If you want to find something or someone, go find them yourself.");
+            vm.linearLayout.addView(secondText);
         } else if (myObjX.contains("what") && myObjX.contains("date") && myObjX.contains("is") || myObjX.matches("date") || myObjX.matches("the date")) {
             secondText.setText("It's April 16, 2019.");
             vm.linearLayout.addView(secondText);
